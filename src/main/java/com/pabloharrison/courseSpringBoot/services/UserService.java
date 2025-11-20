1e3edb885ddf6072946e0dc87262f27ca2ea75b2
PabloHarrison <pabloharrison.77@gmail.com>
2025-11-20 10:50:44 -0300

    Exception handling - update

--- src/main/java/com/pabloharrison/courseSpringBoot/services/UserService.java
+++ src/main/java/com/pabloharrison/courseSpringBoot/services/UserService.java
@@ -4,6 +4,7 @@ import com.pabloharrison.courseSpringBoot.entities.User;
 import com.pabloharrison.courseSpringBoot.repository.UserRepository;
 import com.pabloharrison.courseSpringBoot.services.exceptions.DatabaseException;
 import com.pabloharrison.courseSpringBoot.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -39,9 +40,13 @@ public class UserService {
         }
     }
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
     private void updateData(User entity, User obj) {
         entity.setName(obj.getName());
